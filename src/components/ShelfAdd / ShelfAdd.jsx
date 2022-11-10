7b4150bd1d6fdf8493ff257920178b53f564897e
Rider <goejrider@gmail.com>
2022-11-10 10:20:50 -0600

    Description and url reducers linked to ShelfAdd

--- src/components/ShelfAdd / ShelfAdd.jsx
+++ src/components/ShelfAdd / ShelfAdd.jsx
@@ -3,9 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 
 
 function ShelfAdd() {
+    const dispatch = useDispatch();
+    const desc = useSelector(store=>store.desc);
+    const url = useSelector(store=>store.url);
 
     const submitForm = (e) => {
         e.preventDefault();
+        
     }
     return (
         <>
@@ -14,11 +18,14 @@ function ShelfAdd() {
             <input
             type="text"
             Placeholder="Description"
-            
+            onChange={(e)=>{dispatch({type:'STORE_DESC', payload: e.target.value})}}
+            value={desc}
             ></input>
             <input 
             type="text"
             placeholder="image url"
+            onChange={(e)=>{dispatch({type:'STORE_URL', payload: e.target.value})}}
+            value={url}
             ></input>
             <button type="submit">Add item</button>
         </form>
