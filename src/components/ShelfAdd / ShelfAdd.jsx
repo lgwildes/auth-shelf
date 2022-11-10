16cc85accdceea53dc4f84acd90cedb0456ee1ad
Rider <goejrider@gmail.com>
2022-11-10 10:11:33 -0600

    Form built

--- src/components/ShelfAdd / ShelfAdd.jsx
+++ src/components/ShelfAdd / ShelfAdd.jsx
@@ -1,11 +1,27 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+
 
 
 function ShelfAdd() {
+
+    const submitForm = (e) => {
+        e.preventDefault();
+    }
     return (
         <>
         <h1>ShelfAdd</h1>
+        <form onSubmit={submitForm}>
+            <input
+            type="text"
+            Placeholder="Description"
+            
+            ></input>
+            <input 
+            type="text"
+            placeholder="image url"
+            ></input>
+            <button type="submit">Add item</button>
+        </form>
         </>
     );
 }
